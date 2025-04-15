298dd5eef99d50071953a03a7d9a823f369c8e1f
Jan Adnan Farooq <janadnanfarooq@gmail.com>
2025-04-15 22:43:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 3000;
-const MONGO_URI = "mongodb://localhost:27017/extensionAnalytics"; // Change if using Atlas
+const MONGO_URI = "Enter your own mongodb URI"; 
 
 app.use(cors());
 app.use(express.json());
